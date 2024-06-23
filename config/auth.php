bbe3c821a14ff92817c127a6e882aff0ef0f1197
RSlavov <rumen@eurocoders.com>
2024-06-23 11:47:50 +0300

    configure jwt

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        // API Guard
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ]
     ],
 
     /*
