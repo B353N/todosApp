7e3fd43cbad42c22ea528eeed370d30b85f4400f
RSlavov <rumen@eurocoders.com>
2024-06-23 16:13:11 +0300

    Handle unauthorized error

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Login error response.
+Route::get('/unathorized', function () {
+    return response()->json(['message' => 'Unauthorized'], 401);
+})->name('login');
 // Register a new user.
 Route::post('/register', 'App\Http\Controllers\AuthController@register');
 // Login a user.
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
 
 // Group routes that require authentication.
-Route::middleware('auth:api')->group(function () {
+Route::middleware(['auth:api'])->group(function () {
     // Logout a user.
     Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
     // Refresh a user's token.
